ecd5d39166dc57895b8f81f6f071554317fbdfab
avnishrai569 <105975356+avnishrai569@users.noreply.github.com>
2024-07-22 12:39:54 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bookRoutes = require('./routes/books');
+const favoriteRoutes = require('./routes/favorites');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.set('strictQuery', false); 
+
+mongoose.connect('mongodb://localhost:27017/bookcatalog')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/books', bookRoutes);
+app.use('/api/favorites', favoriteRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
